760c5b800cca99a77649527aa6e1071cd967b3e9
admin <admin@penguin>
2020-01-07 00:07:25 -0500

    For4h Piles

--- build-run.sh
+++ build-run.sh
@@ -1,3 +1,9 @@
+git clone https://github.com/FreeCAD/FreeCAD.git
+cd FreeCAD
+git pull
+cd ..
+
+
 sudo docker kill $(sudo docker ps -q)
 sudo docker build . -t kewl
 sudo docker run -d -p 8080:8080 -p 8000:8000 -p 80:80 kewl
