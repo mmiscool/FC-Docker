1684de7e667fc8bd01b24cfbf41c5b9bac7fbfb2
admin <admin@penguin>
2020-01-08 14:31:32 -0500

    More Good stuffs

--- build-run.sh
+++ build-run.sh
@@ -7,3 +7,7 @@ sudo docker kill $(sudo docker ps -q)
 sudo docker build . -t kewl
 sudo docker run -d -p 80:80 kewl
 
+echo IP address of docker container is
+sudo docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'  $(sudo docker ps -q)
+sleep 10
+firefox $(sudo docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'  $(sudo docker ps -q)) &
